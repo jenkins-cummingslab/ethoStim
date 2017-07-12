8f4e3505266f8a65d22819829894ba11f84b5061
Richie <richie.rausch@gmail.com>
2017-07-12 13:00:20 -0500

    Updated record_only script

--- individualtesting/record_only.py
+++ individualtesting/record_only.py
@@ -10,6 +10,16 @@ ap.add_argument("-f", "--format", help="Format (mjpeg or h264")
 args = vars(ap.parse_args())
     
 camera = picamera.PiCamera()
+camera.resolution = (1296, 972)
+camera.contrast = 100
+camera.brightness = 75
+camera.framerate = 25
+camera.exposure_mode = 'auto'
+camera.awb_mode = 'off'
+camera.awb_gains = (1.8, 1.0)
+camera.led = False
+camera.rotation = 180
+
 if args["format"] == 'h264':
     camera.start_recording(args["filename"] + '.h264', format="h264")
 elif args["format"] == 'mjpeg':
