c3a419dc9c984ac8f871eda201d480aa8fe58f7b
Richie <richie.rausch@gmail.com>
2017-08-09 14:53:20 -0500

    Updated camera settings in record_only.py to match trial.py

--- individualtesting/record_only.py
+++ individualtesting/record_only.py
@@ -8,15 +8,15 @@ ap.add_argument("-l", "--vidlen", help="Length of the video recording (secs)")
 ap.add_argument("-o", "--outfile", help="Filename without the extension")
 ap.add_argument("-f", "--format", help="Format (mjpeg or h264")
 args = vars(ap.parse_args())
-    
+
 camera = picamera.PiCamera()
 camera.resolution = (1296, 972)
 camera.contrast = 100
-camera.brightness = 75
+camera.brightness = 85
 camera.framerate = 25
 camera.exposure_mode = 'auto'
 camera.awb_mode = 'off'
-camera.awb_gains = (1.8, 1.0)
+camera.awb_gains = (2.8, 1.0)
 camera.led = False
 camera.rotation = 180
 
@@ -28,7 +28,7 @@ else:
     print 'ERROR> Invalid format specified, must be h264 or mjpeg'
     print 'Exiting...'
     sys.exit(1)
-    
+
 #TODO: Replace sleep with more accurate timer
 time.sleep(float(args["vidlen"]))
 
